e002b07fd12074d3c948fe0c64091712acffc48a
langker <langker@aliyun.com>
2016-05-16 13:55:41 +0800

    add basic method

--- recordDemo/ViewController.swift
+++ recordDemo/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioRecorder:AVAudioRecorder!
+    var audioPlayer:AVAudioPlayer!
+    let recordSettings = [AVSampleRateKey : NSNumber(float: Float(44100.0)),//声音采样率
+        AVFormatIDKey : NSNumber(int: Int32(kAudioFormatMPEG4AAC)),//编码格式
+        AVNumberOfChannelsKey : NSNumber(int: 1),//采集音轨
+        AVEncoderAudioQualityKey : NSNumber(int: Int32(AVAudioQuality.Medium.rawValue))]//音频质量
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try audioRecorder = AVAudioRecorder(URL: self.directoryURL()!,
+                                                settings: recordSettings)//初始化实例
+            audioRecorder.prepareToRecord()//准备录音
+        } catch {
+        }
+    }
+    func directoryURL() -> NSURL? {
+        //定义并构建一个url来保存音频，音频文件名为ddMMyyyyHHmmss.caf
+        //根据时间来设置存储文件名
+        let currentDateTime = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyyHHmmss"
+        let recordingName = formatter.stringFromDate(currentDateTime)+".caf"
+        print(recordingName)
+        
+        let fileManager = NSFileManager.defaultManager()
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = urls[0] as NSURL
+        let soundURL = documentDirectory.URLByAppendingPathComponent(recordingName)
+        return soundURL
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +51,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func OnStart(sender: AnyObject) {
+        print("memeda");
+        if !audioRecorder.recording {
+            let audioSession = AVAudioSession.sharedInstance()
+            do {
+                try audioSession.setActive(true)
+                audioRecorder.record()
+                print("record!")
+            } catch {
+            
+            }
+        }
+    }
 
+    @IBAction func OnStop(sender: AnyObject) {
+        print("666");
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setActive(false)
+            print("stop!!")
+        } catch {
+        
+        }
+
+    }
+    @IBAction func OnPlay(sender: AnyObject) {
+        print("fehu");
+        //开始播放
+        if (!audioRecorder.recording){
+            do {
+                try audioPlayer = AVAudioPlayer(contentsOfURL: audioRecorder.url)
+                audioPlayer.play()
+                print("play!!")
+            } catch {
+            }
+        }
+    }
 }
 
