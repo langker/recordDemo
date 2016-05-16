171e668539ead90ffe19872eb7d73d7ae2616a09
langker <langker@aliyun.com>
2016-05-16 13:34:20 +0800

    Initial Commit

---
+++ recordDemoTests/recordDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  recordDemoTests.swift
+//  recordDemoTests
+//
+//  Created by langker on 16/5/16.
+//  Copyright © 2016年 langker. All rights reserved.
+//
+
+import XCTest
+@testable import recordDemo
+
+class recordDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
